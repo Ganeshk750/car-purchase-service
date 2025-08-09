88e3ff24c02a36d041d0e0721375b4e06cbbd664
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2025-08-09 14:56:29 +0530

    Add files via upload
    initial commit

---
+++ car-purchase-service/src/main/java/com/hcltech/car_purchase_service/CarPurchaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hcltech.car_purchase_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPurchaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPurchaseServiceApplication.class, args);
+	}
+
+}
---
+++ car-purchase-service/src/test/java/com/hcltech/car_purchase_service/CarPurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcltech.car_purchase_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPurchaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
